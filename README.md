9cd6a5a452c8533ff59f10394b5b323e2e7be397
salty-max <max@jellycat.fr>
2023-12-25 01:36:41 +0100

    initial commit :wave:

---
+++ README.md
@@ -0,0 +1,27 @@
+# PIT
+
+The stupidest content tracker, in python.
+
+## Features
+
+- [ ] add
+- [ ] cat-file
+- [ ] check-ignore
+- [ ] checkout
+- [ ] commit
+- [ ] hash-object
+- [ ] init
+- [ ] log
+- [ ] ls-files
+- [ ] ls-tree
+- [ ] rev-parse
+- [ ] rm
+- [ ] show-ref
+- [ ] status
+- [ ] tag
+
+## Use
+
+```sh
+pit [-v | --version] [-h | --help] <command> [<args>, ...]
+```
---
+++ libpit.py
@@ -0,0 +1,23 @@
+import argparse
+import collections
+import configparser
+from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
+import os
+import re
+import sys
+import zlib
+
+argparser = argparse.ArgumentParser(description="The stupidest content tracker")
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
+
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case _:
+            print("Bad command.")
